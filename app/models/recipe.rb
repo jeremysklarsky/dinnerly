9c61cc4fe71e01cde2f883fddb47c3aec5680b7f
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-09 09:56:34 -0400

    Add callback function to Recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,11 +7,28 @@ class Recipe < ActiveRecord::Base
   # has_many :recipe_cuisines
   # has_many :cuisines, through: :recipe_cuisines
   belongs_to :cuisine
-
+  after_create :assign_course_names
+  
   def course_names
     self.courses.collect{|course| course.name}
   end
   
+  def assign_course_names
+    if !(self.course_names & Course::COURSES["Appetizers"]).empty?
+      self.appetizer = true
+    end
+    if !(self.course_names & Course::COURSES["Main Dishes"]).empty?
+      self.main = true     
+    end
+    if !(self.course_names & Course::COURSES["Side Dishes"]).empty?
+      self.side = true     
+    end
+    if !(self.course_names & Course::COURSES["Desserts"]).empty?
+      self.dessert = true
+    end
+    self.save     
+  end
+
 end
 
 
