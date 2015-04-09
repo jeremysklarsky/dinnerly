fc12b013e2e01d465702810add44982e685c84a6
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-09 11:09:48 -0400

    Begin styling menu form.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,6 +5,19 @@ class Menu < ActiveRecord::Base
   has_many :menu_recipes
   has_many :recipes, through: :menu_recipes
 
-  
+  def appetizers
+    self.menu_recipes.where(course_name: "Appetizer").collect {|mr| mr.recipe}
+  end
 
+  def sides
+    self.menu_recipes.where(course_name: "Side").collect {|mr| mr.recipe}
+  end
+
+  def mains
+    self.menu_recipes.where(course_name: "Main").collect {|mr| mr.recipe}
+  end
+
+  def desserts
+    self.menu_recipes.where(course_name: "Dessert").collect {|mr| mr.recipe}
+  end
 end
