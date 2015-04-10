9504d505ae21d93e8371eda0c48df02de967e574
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-04-10 15:29:38 -0400

    added styling to forms

--- app/assets/javascripts/emailGuests.js
+++ app/assets/javascripts/emailGuests.js
@@ -9,7 +9,7 @@ function addInviteGuestsListener(){
   $('#email-guests-btn').on("click", function(){
     $('form').removeClass("hidden");
     // debugger;
-    $('#email-guests-btn').html("<button id='add-guests-btn'>Add Another Guest</button>");
+    $('#email-guests-btn').replaceWith("<button id='add-guests-btn' class='btn btn-primary'>Add Another Guest</button><br>");
     $("#add-guests-btn").on("click", function(){
       debugger;
       $("#email-forms").append("<input type='text' name='guest[emails][]'/>");
