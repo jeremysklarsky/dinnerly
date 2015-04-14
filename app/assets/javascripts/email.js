dafff3a15648d264c6a1eac93cb52b8d0b15db0e
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-14 16:49:37 -0400

    working invite guests button functionality

---
+++ app/assets/javascripts/email.js
@@ -0,0 +1,15 @@
+$(function(){
+  addCreateMenuListener();
+})
+
+
+
+function addCreateMenuListener(){
+
+  $('#create-menu').on("click", function(){
+    debugger
+    $('.email-guests').removeClass("hidden");
+
+  })
+
+}
