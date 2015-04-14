d5baceb269a13694300d196d46da5ece58c7e382
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-04-14 17:16:04 -0400

    fixed email bug and added content and some styling to email invite

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -2,18 +2,16 @@ class GuestMailer < ApplicationMailer
 
   default from: "approvablefeast@gmail.com"
 
-  def invite_guests(user_email, recipients, subject, dinner_page)
+  def invite_guests(user_email, recipient, subject, dinner_page, dinner)
     @dinner_page = dinner_page
     @subject = subject
+    @dinner = dinner
 
     @user_email = user_email
-    @recipients = recipients
-
-    @recipients.each do |recipient|
-      mail(:to => recipient,
+    @recipient = recipient
+    mail(:to => @recipient,
             :subject => @subject,
             :reply_to => @user_email)
-    end
   end  
 
 end
