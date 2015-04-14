71d6285aa5442f9913f0d7267e3406f7673bd655
ktravers <travers.kt@gmail.com>
2015-04-14 18:21:33 -0400

    add election form styling

--- app/controllers/menu_recipes_controller.rb
+++ app/controllers/menu_recipes_controller.rb
@@ -16,7 +16,7 @@ class MenuRecipesController < ApplicationController
   end
 
   def vote
-    binding.pry
+    # binding.pry
     @menu_recipe = MenuRecipe.find(params[:id])
     unless session[:alert]
       @menu_recipe.votes.build(:user_id => current_user.id)
@@ -32,7 +32,6 @@ class MenuRecipesController < ApplicationController
   private
 
   def can_vote?
-    # binding.pry
     menu_recipe = MenuRecipe.find(params[:id])
     course_name = menu_recipe.course_name
     menu = menu_recipe.menu 
@@ -40,14 +39,12 @@ class MenuRecipesController < ApplicationController
     vote_limit = recipes.count / 2
     total_course_votes = recipes.collect {|r| r.votes}.flatten
     total_user_votes = total_course_votes.select {|v| v.user_id == 1}.size
-    binding.pry
+
     if total_user_votes >= vote_limit
       session[:alert] = "raise alert"
     else
       session[:alert] = nil
     end
- 
-
   end
 
 end
