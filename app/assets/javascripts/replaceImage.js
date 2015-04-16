074f525c8f36f3b87752473f18858d711be46622
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-16 10:47:02 -0400

    users can change password

--- app/assets/javascripts/replaceImage.js
+++ app/assets/javascripts/replaceImage.js
@@ -1,4 +1,4 @@
-$(function() {
+  $(function() {
 
   $('img').error(function(){
    $(this).attr('src', 'http://ctbnutrition.com/wp-content/uploads/2015/03/Vegetable-Question-Mark.jpg');
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
+
+  def update
+    @user = current_user
+    binding.pry
+    if !!@user.authenticate(params[:user][:password])
+      binding.pry
+      @user.name = params[:user][:name]
+      @user.email = params[:user][:email]
+      if !params[:user][:new_password].empty? 
+        @user.password = params[:user][:new_password]
+        @user.password_confirmation = params[:user][:confirm_password]
+        if @user.save
+          flash[:notice] = "User details updated."
+        else
+          flash[:notice] = "Please confirm new password."
+        end
+      else
+        binding.pry
+        @user.save
+        flash[:notice] = "User details updated."
+      end
+    else
+      binding.pry
+      flash[:notice] = "Incorrect password. No User details updated. Click below to reset your password."
+    end
+    render 'edit'
+  end 
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
