ecf7c750f0cc5ff4a707d65d934fc2cb068ead3f
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-17 10:02:43 -0400

    fix appending more email forms to dinner page

--- app/assets/javascripts/emailGuests.js
+++ app/assets/javascripts/emailGuests.js
@@ -7,7 +7,7 @@ function addInviteGuestsListener(){
     $('form').removeClass("hidden");
     $('#email-guests-btn').replaceWith("<button id='add-guests-btn' class='btn btn-primary'>Add Another Guest</button><br>");
     $("#add-guests-btn").on("click", function(){
-      $("#email-forms").append("<input type='text' name='guest[emails][]'/>");
+      $("#email-forms").append("<input type='text' name='guest[emails][]'/><br><br>");
     })
     
   })
