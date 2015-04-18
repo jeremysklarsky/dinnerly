a87100b0bba343ef6fd7c0e219f9ecf1d5f3b48a
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-04-18 17:33:38 -0400

    fix bug w no of hours in countdown

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -7,8 +7,8 @@
         seconds = eventDate - currentDate;
         days = Math.floor(seconds / 86400);
         seconds -= days * 60 * 60 * 24;
-        hours = Math.floor(seconds / 3600);
-        seconds -= hours * 60 * 60;
+        hours = (Math.floor(seconds / 3600)) + 4;
+        seconds -= (hours -4) * 60 * 60;
         minutes = Math.floor(seconds / 60);
         seconds -= minutes * 60;
         days == 1 ? thisEl.find(".timeRefDays").text("day") : thisEl.find(".timeRefDays").text("days");
@@ -27,7 +27,7 @@
             thisEl.find(".minutes").text(minutes);
             thisEl.find(".seconds").text(seconds)
         } else {
-            alert("Invalid date. Example: 30 Tuesday 2013 15:50:00");
+            alert("broken!");
             clearInterval(interval)
         }
     }
