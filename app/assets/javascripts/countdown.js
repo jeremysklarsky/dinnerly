0882c7fe96dfd2143c3effe86c3e2cd4ac4a29b3
ktravers <travers.kt@gmail.com>
2015-04-19 01:20:02 -0400

    update countdown.js based on code climate recommendations

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -1,34 +1,33 @@
 (function (e) {
-
     e.fn.countdown = function (t, n) {
     function i() {
-        eventDate = Date.parse(r.date) / 1e3;
-        currentDate = Math.floor(e.now() / 1e3);
-        seconds = eventDate - currentDate;
-        days = Math.floor(seconds / 86400);
+        var eventDate = Date.parse(r.date) / 1e3;
+        var currentDate = Math.floor(e.now() / 1e3);
+        var seconds = eventDate - currentDate;
+        var days = Math.floor(seconds / 86400);
         seconds -= days * 60 * 60 * 24;
-        hours = (Math.floor(seconds / 3600)) + 4;
+        var hours = (Math.floor(seconds / 3600)) + 4;
         seconds -= (hours -4) * 60 * 60;
-        minutes = Math.floor(seconds / 60);
+        var minutes = Math.floor(seconds / 60);
         seconds -= minutes * 60;
-        days == 1 ? thisEl.find(".timeRefDays").text("day") : thisEl.find(".timeRefDays").text("days");
-        hours == 1 ? thisEl.find(".timeRefHours").text("hour") : thisEl.find(".timeRefHours").text("hours");
-        minutes == 1 ? thisEl.find(".timeRefMinutes").text("minute") : thisEl.find(".timeRefMinutes").text("minutes");
-        seconds == 1 ? thisEl.find(".timeRefSeconds").text("second") : thisEl.find(".timeRefSeconds").text("seconds");
-        if (r["format"] == "on") {
+        days === 1 ? thisEl.find(".timeRefDays").text("day") : thisEl.find(".timeRefDays").text("days");
+        hours === 1 ? thisEl.find(".timeRefHours").text("hour") : thisEl.find(".timeRefHours").text("hours");
+        minutes === 1 ? thisEl.find(".timeRefMinutes").text("minute") : thisEl.find(".timeRefMinutes").text("minutes");
+        seconds === 1 ? thisEl.find(".timeRefSeconds").text("second") : thisEl.find(".timeRefSeconds").text("seconds");
+        if (r["format"] === "on") {
             days = String(days).length >= 2 ? days : "0" + days;
             hours = String(hours).length >= 2 ? hours : "0" + hours;
             minutes = String(minutes).length >= 2 ? minutes : "0" + minutes;
-            seconds = String(seconds).length >= 2 ? seconds : "0" + seconds
+            seconds = String(seconds).length >= 2 ? seconds : "0" + seconds;
         }
         if (!isNaN(eventDate)) {
             thisEl.find(".days").text(days);
             thisEl.find(".hours").text(hours);
             thisEl.find(".minutes").text(minutes);
-            thisEl.find(".seconds").text(seconds)
+            thisEl.find(".seconds").text(seconds);
         } else {
             alert("broken!");
-            clearInterval(interval)
+            clearInterval(interval);
         }
     }
     var thisEl = e(this);
@@ -38,7 +37,7 @@
     };
     t && e.extend(r, t);
     i();
-    interval = setInterval(i, 1e3)
+    var interval = setInterval(i, 1e3);
     }
     })(jQuery);
 
@@ -64,17 +63,15 @@
 
 
 $(function () {
-    
     function e() {
         var e = new Date;
         e.setDate(e.getDate() + 60);
-        dd = e.getDate();
-        mm = e.getMonth() + 1;
-        y = e.getFullYear();
-        futureFormattedDate = mm + "/" + dd + "/" + y;
-        return futureFormattedDate
+        var dd = e.getDate();
+        var mm = e.getMonth() + 1;
+        var y = e.getFullYear();
+        var futureFormattedDate = mm + "/" + dd + "/" + y;
+        return futureFormattedDate;
     };
-
     countDownListener();
 });
 
@@ -97,12 +94,3 @@ function countDownListener(){
 //         });
 //     })
 // };
-
-
-
-
-
-
-
-
-
